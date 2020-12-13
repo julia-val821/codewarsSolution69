89a6567d556dc007f6e2b2c318da414ec2b004c3
JuliaVal821 <julia.kolgatina@gmail.com>
2020-12-12 19:19:25 -0500

    Found the largest integer

---
+++ Maximum Multiple.js
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript
+
+    function maxMultiple(divisor, bound){
+        for (let i = bound; i > 0; i-- ){
+            if( i % divisor === 0){
+                return i;
+            }
+        }
+    }
